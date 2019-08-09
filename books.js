1434a26c77a5082427f6716d75592ae70bae293d
David Daza <03dazal@gmail.com>
2019-08-08 21:59:44 -0500

    scrape through all pages

--- books.js
+++ books.js
@@ -6,20 +6,33 @@ const url = 'http://books.toscrape.com/catalogue/page-1.html';
 const file = path.join(__dirname, 'data/books.json');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
-  const data = await page.evaluate(() => {
-    const products = Array.from(document.querySelectorAll('.product_pod'))
-    const data = products.map(product => {
-      return {
-        title: product.querySelector('h3 a').getAttribute('title'),
-        price: product.querySelector('.product_price .price_color').textContent,
-        cover: location.origin + product.querySelector('img').getAttribute('src').slice(2, )
-      }
-    });
-    return data;
-  });
-  fs.writeFileSync(file, JSON.stringify(data), 'utf8')
+
+  let json = []
+  let next = await page.$('.pager .next a')
+  while (next) {
+    const data = await page.evaluate(getData);
+    json = json.concat(data)
+    const [response] = await Promise.all([
+      page.waitForNavigation(),
+      page.click('.pager .next a'),
+    ]);
+    next = await page.$('.pager .next a')
+  }
+  fs.writeFileSync(file, JSON.stringify(json), 'utf8')
   await browser.close();
 })();
+
+function getData() {
+  const products = Array.from(document.querySelectorAll('.product_pod'))
+  const data = products.map(product => {
+    return {
+      title: product.querySelector('h3 a').getAttribute('title'),
+      price: product.querySelector('.product_price .price_color').textContent,
+      cover: location.origin + product.querySelector('img').getAttribute('src').slice(2, )
+    }
+  });
+  return data;
+}
