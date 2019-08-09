4afb40020208c74b626e917199f99f366343213e
David Daza <03dazal@gmail.com>
2019-08-08 22:18:52 -0500

    lint code

--- books.js
+++ books.js
@@ -6,33 +6,38 @@ const url = 'http://books.toscrape.com/catalogue/page-1.html';
 const file = path.join(__dirname, 'data/books.json');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto(url);
 
-  let json = []
-  let next = await page.$('.pager .next a')
+  let json = [];
+  let next = await page.$('.pager .next a');
   while (next) {
     const data = await page.evaluate(getData);
-    json = json.concat(data)
-    const [response] = await Promise.all([
+    json = json.concat(data);
+    await Promise.all([
       page.waitForNavigation(),
       page.click('.pager .next a'),
     ]);
-    next = await page.$('.pager .next a')
+    next = await page.$('.pager .next a');
   }
-  fs.writeFileSync(file, JSON.stringify(json), 'utf8')
+  fs.writeFileSync(file, JSON.stringify(json), 'utf8');
   await browser.close();
 })();
 
+/**
+ * @return {array}
+ */
 function getData() {
-  const products = Array.from(document.querySelectorAll('.product_pod'))
-  const data = products.map(product => {
+  const products = Array.from(document.querySelectorAll('.product_pod'));
+  const data = products.map((product) => {
     return {
       title: product.querySelector('h3 a').getAttribute('title'),
       price: product.querySelector('.product_price .price_color').textContent,
-      cover: location.origin + product.querySelector('img').getAttribute('src').slice(2, )
-    }
+      cover:
+        location.origin +
+        product.querySelector('img').getAttribute('src').slice(2),
+    };
   });
   return data;
 }
